832ba69983096566627ca7b14a2d60c7cf02f790
torres2235 <43655957+torres2235@users.noreply.github.com>
2024-10-03 15:57:29 -0700

    adding section 8

--- Section7/Challenge/main.cpp
+++ Section7/Challenge/main.cpp
@@ -30,12 +30,46 @@
 */
     
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main() {
     
     // your solution goes here
-
-    cout << endl;
+    vector <int> vector1 {};
+    vector <int> vector2 {};
+    
+    
+    vector1.push_back(10);
+    vector1.push_back(20);
+    cout << vector1.at(0) << endl;
+    cout << vector1.at(1) << endl;
+    cout << vector1.size() << endl;
+    
+    vector2.push_back(100);
+    vector2.push_back(200);
+    cout << vector2.at(0) << endl;
+    cout << vector2.at(1) << endl;
+    cout << vector2.size() << endl;
+    
+    vector <vector<int>> vector2d {};
+    vector2d.push_back(vector1);
+    vector2d.push_back(vector2);
+    
+    cout << vector2d.at(0).at(0) << endl;
+    cout << vector2d.at(0).at(1) << endl;
+    cout << vector2d.at(1).at(0) << endl;
+    cout << vector2d.at(1).at(1) << endl;
+    
+    vector1.at(0) = 1000;
+    cout << vector2d.at(0).at(0) << endl;
+    cout << vector2d.at(0).at(1) << endl;
+    cout << vector2d.at(1).at(0) << endl;
+    cout << vector2d.at(1).at(1) << endl;
+    cout << vector1.at(0) << endl;
+    cout << vector1.at(1) << endl;
+    
     return 0;
 }
 
